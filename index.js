f4602de7ad9689cb0ed02b0fb2a6cf5fc73a01d6
Bruno-spj <127638326+Bruno-spj@users.noreply.github.com>
2023-05-13 22:43:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+    
+const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/sun.png");
+              
+if(modoEscuroEstaAtivo){
+
+body.classList.remove("modo-escuro");
+
+} else {
+body.classList.add("modo-escuro");
+
+imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/moon.png");
+
+
+}
+
+});
